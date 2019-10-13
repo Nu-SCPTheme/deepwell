c59197674e7211d67e5eea41b9c9dcbab9280300
Ammon Smith <ammon.i.smith@gmail.com>
2019-10-13 02:33:36 -0400

    Remove unused import.

--- src/revisions/data.rs
+++ src/revisions/data.rs
@@ -18,7 +18,7 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-use git2::{Blame as GitBlame, Oid};
+use git2::Oid;
 
 #[derive(Debug, Copy, Clone)]
 pub struct CommitInfo<'a> {
