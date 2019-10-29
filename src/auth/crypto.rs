ad989773779f9e9782ecb4cdd31ee0a5aa6c93be
Ammon Smith <ammon.i.smith@gmail.com>
2019-10-29 00:18:59 -0400

    Add new_password().

--- src/auth/crypto.rs
+++ src/auth/crypto.rs
@@ -18,6 +18,9 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
+use super::models::*;
+use crate::user::UserId;
+use crate::Result;
 use crypto::scrypt::{scrypt, ScryptParams};
 use crypto::util::fixed_time_eq;
 use rand::{rngs::OsRng, RngCore};
@@ -26,12 +29,45 @@ const PARAM_LOGN: u8 = 13;
 const PARAM_R: u32 = 8;
 const PARAM_P: u32 = 16;
 
+type Hash = [u8; 32];
+type Salt = [u8; 16];
+
 lazy_static! {
     static ref PARAMS: ScryptParams = ScryptParams::new(PARAM_LOGN, PARAM_R, PARAM_P);
 }
 
-fn random_salt() -> [u8; 16] {
+#[inline]
+fn make_model<'a>(user_id: UserId, hash: &'a [u8], salt: &'a [u8]) -> NewPassword<'a> {
+    NewPassword {
+        user_id: user_id.into(),
+        hash,
+        salt,
+        logn: PARAM_LOGN.into(),
+        param_r: PARAM_R as i32,
+        param_p: PARAM_P as i32,
+    }
+}
+
+fn random_salt() -> Salt {
     let mut bytes = [0; 16];
     OsRng.fill_bytes(&mut bytes);
     bytes
+}
+
+#[inline]
+fn new_hash() -> Hash {
+    [0; 32]
+}
+
+pub fn new_password<F>(user_id: UserId, password: &[u8], f: F) -> Result<()>
+where
+    F: FnOnce(NewPassword<'_>) -> Result<()>,
+{
+    let salt = random_salt();
+    let mut hash = new_hash();
+
+    scrypt(password, &salt, &*PARAMS, &mut hash);
+
+    let model = make_model(user_id, &hash, &salt);
+    f(model)
 }
