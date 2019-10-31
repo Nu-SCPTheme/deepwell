60f6737cdee0e55ca6bb27b22ce4e5f6d02689c4
Ammon Smith <ammon.i.smith@gmail.com>
2019-10-31 11:43:57 -0400

    Update sequence after default users.
    Also format that migration.

--- migrations/2019-10-18-151322_default_users/down.sql
+++ migrations/2019-10-18-151322_default_users/down.sql
@@ -1,1 +1,2 @@
 DELETE FROM users WHERE user_id IN (0, 1, 2, 3, 4, 5);
+ALTER SEQUENCE users_user_id_seq RESTART WITH 1;
--- migrations/2019-10-18-151322_default_users/up.sql
+++ migrations/2019-10-18-151322_default_users/up.sql
@@ -1,18 +1,73 @@
 INSERT INTO users (user_id, name, email, is_verified, is_bot, website, about, location)
-VALUES (0, 'unknown', 'unknown@example.com', true, true, 'https://example.com/', 'Standard account for unknown users', 'unknown');
+    VALUES (
+        0,
+        'unknown',
+        'unknown@example.com',
+        true,
+        true,
+        'https://example.com/',
+        'Standard account for unknown users',
+        'unknown'
+    );
 
 INSERT INTO users (user_id, name, email, is_verified, is_bot, website, about, location)
-VALUES (1, 'administrator', 'noreply@example.com', true, true, 'https://example.com/', 'Standard account for root-level access', 'Site-01');
+    VALUES (
+        1,
+        'administrator',
+        'noreply@example.com',
+        true,
+        true,
+        'https://example.com/',
+        'Standard account for root-level access',
+        'Site-01'
+    );
 
 INSERT INTO users (user_id, name, email, is_verified, is_bot, website, about, location)
-VALUES (2, 'system', 'system@example.com', true, true, 'https://example.com/', 'Standard account for system actions', 'everywhere');
+    VALUES (
+        2,
+        'system',
+        'system@example.com',
+        true,
+        true,
+        'https://example.com/',
+        'Standard account for system actions',
+        'everywhere'
+    );
 
 INSERT INTO users (user_id, name, email, is_verified, is_bot, website, about, location)
-VALUES (3, 'anonymous', 'anonymous@example.com', true, true, 'https://example.com/', 'Standard account for anonymous users', 'unknown');
-
+    VALUES (
+        3,
+        'anonymous',
+        'anonymous@example.com',
+        true,
+        true,
+        'https://example.com/',
+        'Standard account for anonymous users',
+        'unknown'
+    );
 
 INSERT INTO users (user_id, name, email, is_verified, is_bot, website, about, location)
-VALUES (4, 'deleted', 'deleted@example.com', true, true, 'https://example.com/', 'Standard account for deleted users', 'deleted');
+    VALUES (
+        4,
+        'deleted',
+        'deleted@example.com',
+        true,
+        true,
+        'https://example.com/',
+        'Standard account for deleted users',
+        'deleted'
+    );
 
 INSERT INTO users (user_id, name, email, is_verified, is_bot, website, about, location)
-VALUES (5, 'nobody', 'nobody@example.com', true, true, 'https://example.com/', 'Standard account for unprivileged users', '?');
+    VALUES (
+        5,
+        'nobody',
+        'nobody@example.com',
+        true,
+        true,
+        'https://example.com/',
+        'Standard account for unprivileged users',
+        '?'
+    );
+
+ALTER SEQUENCE users_user_id_seq RESTART WITH 6;
