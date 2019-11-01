cae6d1791f60b1921089a7e51ae902c9e186bfef
Ammon Smith <ammon.i.smith@gmail.com>
2019-11-01 01:42:38 -0400

    Fix down migration.

--- migrations/2019-10-18-151322_default_users/down.sql
+++ migrations/2019-10-18-151322_default_users/down.sql
@@ -1,3 +1,3 @@
-DELETE FROM users WHERE user_id IN (0, 1, 2, 3, 4, 5);
 DELETE FROM passwords WHERE user_id IN (0, 1, 2, 3, 4, 5);
+DELETE FROM users WHERE user_id IN (0, 1, 2, 3, 4, 5);
 ALTER SEQUENCE users_user_id_seq RESTART WITH 1;
