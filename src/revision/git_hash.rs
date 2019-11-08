99b66d607e7d3c51dfef408d93400208108d1105
Ammon Smith <ammon.i.smith@gmail.com>
2019-11-08 00:49:14 -0500

    Fix stack overflow.
    🤦

--- src/revision/git_hash.rs
+++ src/revision/git_hash.rs
@@ -88,6 +88,6 @@ impl Debug for GitHash {
 
 impl Display for GitHash {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{}", &self)
+        write!(f, "{}", &self.0)
     }
 }
