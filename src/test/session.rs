48f763d076b9c7222dc4cc5653a5a6722ac6c5db
Ammon Smith <ammon.i.smith@gmail.com>
2020-01-26 20:46:16 -0500

    Update tests.

--- src/test/session.rs
+++ src/test/session.rs
@@ -48,7 +48,7 @@ fn session_manager() {
         task::block_on(async {
             let user_id = setup(server).await;
             session_manager_internal_id(server, user_id).await;
-            session_manager_internal_name(server).await;
+            session_manager_internal_name(server, user_id).await;
         })
     });
 }
@@ -94,20 +94,23 @@ async fn session_manager_internal_id(server: &Server, user_id: UserId) {
     let second = &attempts[1];
     let third = &attempts[2];
 
-    assert_eq!(first.user_id(), user_id);
+    assert_eq!(first.user_id(), Some(user_id));
+    assert_eq!(first.username_or_email(), None);
     assert_eq!(first.ip_address(), *IP_ADDRESS_2);
     assert_eq!(first.success(), false);
 
-    assert_eq!(second.user_id(), user_id);
+    assert_eq!(second.user_id(), Some(user_id));
+    assert_eq!(second.username_or_email(), None);
     assert_eq!(second.ip_address(), *IP_ADDRESS_1);
     assert_eq!(second.success(), false);
 
-    assert_eq!(third.user_id(), user_id);
+    assert_eq!(third.user_id(), Some(user_id));
+    assert_eq!(third.username_or_email(), None);
     assert_eq!(third.ip_address(), *IP_ADDRESS_1);
     assert_eq!(third.success(), true);
 }
 
-async fn session_manager_internal_name(server: &Server) {
+async fn session_manager_internal_name(server: &Server, user_id: UserId) {
     // Login
     let error = server
         .try_login("squirrel", "blackmoonhowls", *IP_ADDRESS_1)
@@ -148,5 +151,52 @@ async fn session_manager_internal_name(server: &Server) {
         .expect("Unable to login");
 
     // Check all login attempts
-    // TODO
+    let date = NaiveDate::from_ymd(2001, 1, 1).and_hms(6, 0, 0);
+    let attempts = server
+        .get_all_login_attempts(DateTime::from_utc(date, Utc))
+        .await
+        .expect("Unable to get login attempts");
+
+    // Ignore entries from previous test
+
+    let attempts = &attempts[3..];
+
+    assert_eq!(attempts.len(), 6);
+
+    let first = &attempts[0];
+    let second = &attempts[1];
+    let third = &attempts[2];
+    let fourth = &attempts[3];
+    let fifth = &attempts[4];
+    let sixth = &attempts[5];
+
+    assert_eq!(first.user_id(), None);
+    assert_eq!(first.username_or_email(), Some("squirrel"));
+    assert_eq!(first.ip_address(), *IP_ADDRESS_1);
+    assert_eq!(first.success(), false);
+
+    assert_eq!(second.user_id(), Some(user_id));
+    assert_eq!(second.username_or_email(), None);
+    assert_eq!(second.ip_address(), *IP_ADDRESS_1);
+    assert_eq!(second.success(), false);
+
+    assert_eq!(third.user_id(), Some(user_id));
+    assert_eq!(third.username_or_email(), None);
+    assert_eq!(third.ip_address(), *IP_ADDRESS_2);
+    assert_eq!(third.success(), true);
+
+    assert_eq!(fourth.user_id(), None);
+    assert_eq!(fourth.username_or_email(), Some("jenny@gmail.com"));
+    assert_eq!(fourth.ip_address(), *IP_ADDRESS_1);
+    assert_eq!(fourth.success(), false);
+
+    assert_eq!(fifth.user_id(), Some(user_id));
+    assert_eq!(fifth.username_or_email(), None);
+    assert_eq!(fifth.ip_address(), *IP_ADDRESS_1);
+    assert_eq!(fifth.success(), false);
+
+    assert_eq!(sixth.user_id(), Some(user_id));
+    assert_eq!(sixth.username_or_email(), None);
+    assert_eq!(sixth.ip_address(), *IP_ADDRESS_2);
+    assert_eq!(sixth.success(), true);
 }
