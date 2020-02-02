41559689bf45af165bef458dca4bca99e934dc1f
Ammon Smith <ammon.i.smith@gmail.com>
2020-02-01 22:14:52 -0500

    Only run postgres on linux.

---
+++ .travis/database.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -eu
+
+if [[ $TRAVIS_OS_NAME != linux ]]; then
+	echo 'Skipping database, only running on linux'
+	exit 0
+fi
+
+case "$1" in
+	setup)
+		cargo install diesel_cli --no-default-features --features postgres
+		psql -c "CREATE USER overseer PASSWORD 'blackmoon';" -U postgres
+		psql -c "CREATE DATABASE deepwell OWNER overseer;" -U postgres
+		;;
+	check)
+		diesel migration run
+		diesel migration redo
+		;;
+esac
