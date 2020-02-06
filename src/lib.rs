ed3c943459a4bf959d3d640a77b32267f26be89e
Ammon Smith <ammon.i.smith@gmail.com>
2020-02-05 23:57:00 -0500

    Add Session to exported models.

--- src/lib.rs
+++ src/lib.rs
@@ -80,7 +80,7 @@ pub mod model {
     pub use crate::page::Page;
     pub use crate::rating::Rating;
     pub use crate::revision::{Blame, GitHash};
-    pub use crate::session::LoginAttempt;
+    pub use crate::session::{LoginAttempt, Session};
     pub use crate::user::User;
     pub use crate::wiki::{Wiki, WikiSettings};
 }
