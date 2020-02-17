1244030cec99bceeb1f67c92ab65c1e09dbfa989
Ammon Smith <ammon.i.smith@gmail.com>
2020-02-17 01:15:31 -0500

    Use postgresql 12 on travis.

--- .travis/database.sh
+++ .travis/database.sh
@@ -8,6 +8,13 @@ fi
 
 case "$1" in
 	setup)
+		# Setup postgres 12
+		sudo sed -i 's/port = 5433/port = 5432/' /etc/postgresql/12/main/postgresql.conf
+		sudo cp /etc/postgresql/{9.6,12}/main/pg_hba.conf
+		sudo service postgresql stop
+		sudo service postgresql start 12
+
+		# Setup database
 		cargo install diesel_cli --no-default-features --features postgres
 		psql -c "CREATE USER overseer PASSWORD 'blackmoon';" -U postgres
 		psql -c "CREATE DATABASE deepwell OWNER overseer;" -U postgres
