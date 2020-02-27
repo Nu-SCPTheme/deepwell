799a852142f1fca4ebe766ff2a299db1f57c2fe7
Ammon Smith <ammon.i.smith@gmail.com>
2020-02-27 01:03:27 -0500

    Add remaining tables to default_users down.sql

--- migrations/2019-10-18-151322_default_users/down.sql
+++ migrations/2019-10-18-151322_default_users/down.sql
@@ -1,6 +1,12 @@
-DELETE FROM passwords WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
-DELETE FROM authors WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
+DELETE FROM wiki_membership WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
+DELETE FROM role_membership WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
+DELETE FROM page_locks WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
+DELETE FROM revisions WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
 DELETE FROM ratings WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
 DELETE FROM ratings_history WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
+DELETE FROM authors WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
+DELETE FROM passwords WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
+DELETE FROM login_attempts WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
+DELETE FROM sessions WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
 DELETE FROM users WHERE user_id IN (0, 1, 2, 3, 4, 5, 6);
 ALTER SEQUENCE users_user_id_seq RESTART WITH 1;
