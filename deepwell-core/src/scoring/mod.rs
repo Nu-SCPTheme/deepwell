4dc4025419155febfca2e4c2154b875104190451
Ammon Smith <ammon.i.smith@gmail.com>
2020-03-08 00:50:12 -0500

    Export all scorers.

--- deepwell-core/src/scoring/mod.rs
+++ deepwell-core/src/scoring/mod.rs
@@ -74,8 +74,11 @@ mod prelude {
     }
 }
 
+pub use self::average::AverageScorer;
 pub use self::null::NullScoring;
+pub use self::percent::PercentScoring;
 pub use self::wikidot::WikidotScoring;
+pub use self::wilson::WilsonScoring;
 
 /// Trait for determining the rating from votes.
 ///
